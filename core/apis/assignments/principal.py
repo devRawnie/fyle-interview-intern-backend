d9b36453ac835f543b019efd51a40fac7dd04f4c
ROHIT SHARMA <43227329+devRawnie@users.noreply.github.com>
2024-09-25 18:58:03 +0530

    hotfix: fix import

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
+from core.models.assignments import Assignment, AssignmentStateEnum
 
-from .schema import AssignmentSchema, AssignmentGradeSchema, AssignmentStateEnum
+from .schema import AssignmentSchema, AssignmentGradeSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 
