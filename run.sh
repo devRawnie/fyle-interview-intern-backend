b0240e8d85c6cc7aff19848ae5d7fec087694887
Rohit <rohitsdec4@gmail.com>
2024-09-25 22:33:25 +0530

    add steps to run migrations as a part of run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,11 @@ set -e
 # Run required migrations
 export FLASK_APP=core/server.py
 
+rm -f core/store.sqlite3
+
 # flask db init -d core/migrations/
 # flask db migrate -m "Initial migration." -d core/migrations/
-# flask db upgrade -d core/migrations/
+flask db upgrade -d core/migrations/
 
 # Run server
 gunicorn -c gunicorn_config.py core.server:app
